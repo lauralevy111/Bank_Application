112074283b76cc438e94a008c2b996fb41c2a385
lauralevy510 <llevy510@gmail.com>
2021-02-27 16:12:36 -0500

    added todo's to Account.java

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,4 +1,10 @@
 package com.company;
 
-public abstract class Account {
+public abstract class Account implements IRate {
+
+    //TODO: list commoon properties for savings and checking acccounts.
+
+    //TODO: constructor: setbase properties and initialize the account
+
+    //TODO: List common methods
 }
