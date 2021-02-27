e2b8e10993eacae42072eb4f4e8e33fefe08adb0
lauralevy510 <llevy510@gmail.com>
2021-02-27 16:18:36 -0500

    added instance of Checking to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Checking checkingAccount1 = new Checking();
 
         //TODO: Read a csv file then create new accounts based on that data
     }
