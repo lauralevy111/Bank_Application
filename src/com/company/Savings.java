160df26b9c04f94ad54ac8788f936dcf62e7c48d
lauralevy510 <llevy510@gmail.com>
2021-02-27 16:14:25 -0500

    added todo's to Savings.java

--- src/com/company/Savings.java
+++ src/com/company/Savings.java
@@ -2,5 +2,9 @@ package com.company;
 
 public class Savings extends Account{
 
+    //TODO: List properties specific to the Savings account
 
+    //TODO: constructor : initialize settings for the savings properties
+
+    //TODO: list any methods specific to savings accounts
 }
