0900a97684748319c18fa1cf0a594d08bda3217a
lauralevy510 <llevy510@gmail.com>
2021-02-27 16:12:54 -0500

    added todo's to Checking.java

--- src/com/company/Checking.java
+++ src/com/company/Checking.java
@@ -2,4 +2,9 @@ package com.company;
 
 public class Checking extends Account{
 
+    //TODO: list properties specific to cheking accounts
+
+    //TODO: constructor: initialize checking account properties
+
+    //TODO:list any methods specifiic to the checking account
 }
