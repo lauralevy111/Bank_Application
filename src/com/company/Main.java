4e435db5743c3609d22ca56e20924a32455d25c1
lauralevy510 <llevy510@gmail.com>
2021-02-28 13:52:20 -0500

    commented out obj creations in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Checking checkingAccount1 = new Checking("Celine Dion");
-        Savings savingsAccount1 = new Savings("Gloria Steinem");
+        //Checking checkingAccount1 = new Checking("Celine Dion");
+        //Savings savingsAccount1 = new Savings("Gloria Steinem");
+
+
         //TODO: Read a csv file then create new accounts based on that data
     }
 }
