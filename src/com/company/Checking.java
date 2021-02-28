d3000734def144a5b6819a91245ee8685745db28
lauralevy510 <llevy510@gmail.com>
2021-02-28 13:38:36 -0500

    added arg to Checking obj: name

--- src/com/company/Checking.java
+++ src/com/company/Checking.java
@@ -2,11 +2,15 @@ package com.company;
 
 public class Checking extends Account{
 
+    public String name;
+
     //TODO: list properties specific to cheking accounts
 
     //constructor: initialize checking account properties
-    public Checking(){
+    public Checking(String name){
+        this.name = name;
         System.out.println("NEW CHECKING ACCOUNT");
+        System.out.println("NAME: " + this.name);
     }
 
     //TODO:list any methods specifiic to the checking account
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Checking checkingAccount1 = new Checking();
+        Checking checkingAccount1 = new Checking("Celine Dion");
 
         //TODO: Read a csv file then create new accounts based on that data
     }
