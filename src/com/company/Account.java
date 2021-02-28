b6281dff2a14117551aeadbd57d468127b007cda
lauralevy510 <llevy510@gmail.com>
2021-02-28 13:59:06 -0500

    added a bunch of feilds for Account.ajvaa

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,8 +1,13 @@
 package com.company;
 
 public abstract class Account implements IRate {
-    //list common properties for savings and checking acccounts.
+    //list common properties for savings and checking accounts.
     public String name;
+    public String sSN;
+    public int balance;
+
+    public String accountNumber;
+    public double rate;
 
     //constructor: setbase properties and initialize the account
     public Account (String name) {
