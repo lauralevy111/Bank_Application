a0a1e9f598fea0bda4eb9e04b64fd3a6bc937afe
lauralevy510 <llevy510@gmail.com>
2021-02-28 13:32:40 -0500

    created construtor in Account.java

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -4,7 +4,10 @@ public abstract class Account implements IRate {
 
     //TODO: list commoon properties for savings and checking acccounts.
 
-    //TODO: constructor: setbase properties and initialize the account
+    //constructor: setbase properties and initialize the account
+    public Account () {
+        System.out.println("NEW ACCOUNT: ");
+    }
 
     //TODO: List common methods
 }
