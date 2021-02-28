f36578fcf1e0326890196a50b4e91b929fd1cc0b
lauralevy510 <llevy510@gmail.com>
2021-02-28 13:48:46 -0500

    created Savings obj in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Checking checkingAccount1 = new Checking("Celine Dion");
-
+        Savings savingsAccount1 = new Savings("Gloria Steinem");
         //TODO: Read a csv file then create new accounts based on that data
     }
 }
