746d5d25901f69c92beb9d0398efacaf3e6287be
lauralevy510 <llevy510@gmail.com>
2021-02-28 13:42:00 -0500

    removed string from Checking.java and put it in Account.java bc its more appropriate theere

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -2,11 +2,17 @@ package com.company;
 
 public abstract class Account implements IRate {
 
+    public String name;
+
     //TODO: list commoon properties for savings and checking acccounts.
 
     //constructor: setbase properties and initialize the account
-    public Account () {
-        System.out.println("NEW ACCOUNT: ");
+    public Account (String name) {
+        this.name = name;
+
+        System.out.println("NEW ACCOUNT " +
+                "\n*NAME: "+ this.name);
+
     }
 
     //TODO: List common methods
--- src/com/company/Checking.java
+++ src/com/company/Checking.java
@@ -2,15 +2,11 @@ package com.company;
 
 public class Checking extends Account{
 
-    public String name;
-
     //TODO: list properties specific to cheking accounts
 
     //constructor: initialize checking account properties
-    public Checking(String name){
-        this.name = name;
+    public Checking(){
         System.out.println("NEW CHECKING ACCOUNT");
-        System.out.println("NAME: " + this.name);
     }
 
     //TODO:list any methods specifiic to the checking account
