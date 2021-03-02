cae6a76d14c1e264fda1c9f4f7aa25bc9d4911a1
lauralevy510 <llevy510@gmail.com>
2021-03-02 10:19:35 -0500

    created LInkedList data & buffered reader br on filePath

--- src/com/company/CSV.java
+++ src/com/company/CSV.java
@@ -1,12 +1,17 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CSV {
 
     //function that reads data from CSV file & returns a list
-    public List<String[]> read(String filePath){
+    public static List<String[]> read(String filePath){
+        List<String[]> data = new LinkedList<String []>();
 
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
     }
 
 }
