179c96d99763eb224596605dae61f76a8590ed05
lauralevy510 <llevy510@gmail.com>
2021-03-03 07:08:21 -0500

    if for accountType & prints the type we're opening

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,11 +18,24 @@ public class Main {
         // Read a csv file then create new accounts based on that data
         String filePath ="/Users/lauralevy/IdeaProjects/Bank_Application/data/NewBankAccounts_202003020954";
         List<String[]> newAccountHolders = CSV.read(filePath);
+
         for(String[] accountHolder: newAccountHolders){
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            int initDeposit = Integer.parseInt(accountHolder[3]);
+
+            System.out.println("-" + name + " " + sSN + " " + accountType + " $"+ initDeposit);
+
+            if(accountType.equals("Savings")) {
+                System.out.println("OPEN A SAVINGS ACCOUNT");
+            }
+            else if(accountType.equals("Checking")){
+                System.out.println("OPEN A SAVINGS ACCOUNT");
+            }
+            else{
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
         }
     }
 }
