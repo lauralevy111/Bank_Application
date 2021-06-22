040c8408dc727500c9fbb6446da7557a833659cc
lauralevy510 <llevy510@gmail.com>
2021-06-22 08:24:18 -0400

    mapping issue resolving

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
         List<Account> accounts = new LinkedList<Account>();
 
         // Read a csv file then create new accounts based on that data
-        String filePath ="/Users/lauralevy/IdeaProjects/Bank_Application/data/NewBankAccounts_202003020954";
+        String filePath ="/Users/lauralevy/IdeaProjects/Bank_Application/data/NewBankAccounts_202003020954;
         List<String[]> newAccountHolders = CSV.read(filePath);
 
         for(String[] accountHolder: newAccountHolders){
