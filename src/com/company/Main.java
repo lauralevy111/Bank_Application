3919af4428a60ff361b5a5e94d0ee5c7425d57f6
lauralevy510 <llevy510@gmail.com>
2021-09-14 17:43:15 -0400

    minr change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -38,7 +38,7 @@ public class Main {
         }
 
         for(Account acc: accounts){
-            System.out.println("****************");
+            System.out.println("***************");
             acc.showInfo();
         }
 
